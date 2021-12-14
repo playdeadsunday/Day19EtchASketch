234e97a6a5afcf951975e0d912e0c2c48718599f
playdeadsunday <aniketc713@gmail.com>
2021-12-14 18:11:01 +0530

    Final

---
+++ main.py
@@ -0,0 +1,35 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forward():
+    tim.forward(10)
+
+
+def move_backward():
+    tim.backward(10)
+
+
+def turn_left():
+    tim.left(5)
+
+
+def turn_right():
+    tim.right(5)
+
+
+def clear():
+    tim.clear()
+    tim.reset()
+
+
+screen.listen()
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear, "c")
+
+screen.exitonclick()
